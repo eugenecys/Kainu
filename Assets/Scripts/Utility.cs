c6ceab3bf5e17b470e81e4c09dfb74500c1e4ae4
Eugene Chiong <eugenecys@gmail.com>
2016-05-04 22:39:02 -0400

    Initial commit of utility file with a gaussian function

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utility {
+    
+    public static float Gaussian(float x, float mu, float sigma)
+    {
+        return Mathf.Exp(Mathf.Pow(-(x - mu), 2) / (2 * Mathf.Pow(sigma, 2))) / Mathf.Sqrt(2 * Mathf.PI * Mathf.Pow(sigma, 2));
+
+    }
+}
