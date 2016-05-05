9f9f0fbee4df0613d26dbb5fb6f7e1ae077024c2
Eugene Chiong <eugenecys@gmail.com>
2016-05-04 21:42:50 -0400

    Initial commit of template code

---
+++ Assets/Scripts/Commandable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Commandable {
+
+    void move(Vector3 _position);
+    void fetch(Fetchable fObj);
+
+}
---
+++ Assets/Scripts/Kainu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Kainu : Singleton<Kainu>, Commandable {
+
+    public void fetch(Fetchable fObj)
+    {
+        //Visually track fObj, gradual spherical interpolation to path plot until fObj hits ground
+    }
+
+    public void move(Vector3 _position)
+    {
+        //Moves to specified position through spherical interpolation of path plots
+    }
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
