c972bf112de4d0a17e19e04033554c8f1fa0ea8e
Eugene Chiong <eugenecys@gmail.com>
2016-05-04 22:09:13 -0400

    Initial commit of hand controller for kainu

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SixenseHand))]
+
 public class HandController : MonoBehaviour {
+
+    public enum Side
+    {
+        Left,
+        Right
+    }
+
+    public Side side;
+
+    private SixenseHand sxHand;
+
+    #region UnityDefaults
+
+    void Awake ()
+    {
+        sxHand = GetComponent<SixenseHand>();
+    }
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    #endregion
+
+    #region Kainu_Interactions
+
+    void grab()
+    {
+
+    }
+
+    void throwObject(Fetchable fObj, Vector3 vect)
+    {
+
+    }
+
+    #endregion
 }
