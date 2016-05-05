a0db2b68f9e61c6b6ecbd8f5dbed06e925be0245
Eugene Chiong <eugenecys@gmail.com>
2016-05-04 22:59:08 -0400

    Initial movement code commit

--- Assets/Scripts/Kainu.cs
+++ Assets/Scripts/Kainu.cs
@@ -9,11 +9,28 @@ public class Kainu : Singleton<Kainu>, Commandable {
         //Visually track fObj, gradual spherical interpolation to path plot until fObj hits ground
     }
 
+    #region Movement_mechanics
+
     public void move(Vector3 _position)
     {
         //Moves to specified position through spherical interpolation of path plots
+        desination = _position;
     }
+
+    private float turningStrength = 0.08f;
+    private Vector3 desination;
     
+    //TODO: Add updating heading vector when turning 
+
+    private Vector3 getHeadingVector()
+    {
+        Vector3 bodyDirection = transform.forward;
+        Vector3 targetDirection = desination;
+        return (1 - turningStrength) * bodyDirection + turningStrength * desination;
+    }
+
+    #endregion
+
     // Use this for initialization
     void Start () {
 	
