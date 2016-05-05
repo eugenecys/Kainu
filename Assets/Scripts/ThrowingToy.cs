c2b4ac6c597b7c62fa46d7710d6023414679338b
Eugene Chiong <eugenecys@gmail.com>
2016-05-04 22:08:45 -0400

    Added throwing toy template class

---
+++ Assets/Scripts/ThrowingToy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class ThrowingToy : MonoBehaviour, Fetchable, Throwable {
+
+    Rigidbody rb;
+
+    #region Unity_Defaults
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        
+    }
+
+    #endregion
+
+    #region Throwing_Mechanics
+
+    public void setThrow(Vector3 vect)
+    {
+        rb.velocity = vect;
+    }
+
+    #endregion
+}
